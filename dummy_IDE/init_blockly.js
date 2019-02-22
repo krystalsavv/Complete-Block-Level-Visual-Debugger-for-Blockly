4512edf3b93a1d1cb0b5a0ceef5fbecba76c3c26
Krystallia Savvaki <csd3380@csd.uoc.gr>
2019-02-22 15:48:36 +0200

    load starting block from xml file

--- dummy_IDE/init_blockly.js
+++ dummy_IDE/init_blockly.js
@@ -2,24 +2,39 @@ import './events.js';
 import '../generator/blockly/blockly.js';
 import {Blockly_Debugger} from '../debugger/debugger.js'; 
 
-window.workspace = {};
+window.workspace = {};  
+     
+window.workspace["blockly1"] = Blockly.inject(
+    'blocklyDiv',
+    {
+            media: '../../media/',
+             toolbox: document.getElementById('toolbox')
+    }
+);
+window.workspace["blockly1"].systemEditorId = 'blockly1';
 
-window.workspace["blockly1"] = Blockly.inject('blocklyDiv',
-	{media: '../../media/',
-	 toolbox: document.getElementById('toolbox')});
-window.workspace["blockly1"].systemEditorId = 'blockly1';	 
+window.workspace["blockly2"] = Blockly.inject(
+    'blocklyDiv2',
+    {
+            media   : '../../media/',
+            toolbox    : document.getElementById('toolbox')
+    }
+);
 
- Blockly.Xml.domToWorkspace(document.getElementById('startBlocks'), 
-	window.workspace["blockly1"]);
+window.workspace["blockly2"].systemEditorId = 'blockly2';    
+
+addEventListener("loadStartingBlocks",function (){
+    
+    Blockly.Xml.domToWorkspace(
+        document.getElementById('startBlocks'), 
+        window.workspace["blockly1"]
+    );
+    
+    Blockly.Xml.domToWorkspace(
+        document.getElementById('startBlocks'), 
+        window.workspace["blockly2"]
+    );
+});
 
 
-
-window.workspace["blockly2"] = Blockly.inject('blocklyDiv2',
-	{media: '../../media/',
-	 toolbox: document.getElementById('toolbox')});
-window.workspace["blockly2"].systemEditorId = 'blockly2';	 
-
- Blockly.Xml.domToWorkspace(document.getElementById('startBlocks'), 
-	window.workspace["blockly2"]);
-
-	//Blockly_Debugger.actions["Variables"].init();
+//Blockly_Debugger.actions["Variables"].init();
